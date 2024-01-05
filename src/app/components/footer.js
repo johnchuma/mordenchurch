738acd476a4fdaf8cf9e0e417d6d8bfa78be5650
johnchuma <johnvchuma@gmail.com>
2024-01-05 19:39:53 +0300

    save

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -54,15 +54,15 @@ const Footer = () => {
 
       <div className="py-16 w-11/12 mx-auto grid grid-cols-1 md:grid-cols-2  gap-y-6">
         <div className="text-sm order-2 md:order-1 text-center md:text-start">
-        Copyright © {year} Morden Church. All rights reserved.
+        {`Copyright ©️ ${year} Morden Church. All rights reserved.`}
         </div>
         <div className="flex space-x-4 justify-center md:justify-end order-1 md:order-2">
-          <div>
+          <div className="cursor-pointer">
           <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className="h-5 w-5 " width="100" height="100" viewBox="0 0 50 50">
 <path d="M 6.9199219 6 L 21.136719 26.726562 L 6.2285156 44 L 9.40625 44 L 22.544922 28.777344 L 32.986328 44 L 43 44 L 28.123047 22.3125 L 42.203125 6 L 39.027344 6 L 26.716797 20.261719 L 16.933594 6 L 6.9199219 6 z"></path>
 </svg>
           </div>
-          <div>
+          <div className="cursor-pointer">
           <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className="h-5 w-5 " width="100" height="100" viewBox="0 0 50 50">
     <path d="M 16 3 C 8.83 3 3 8.83 3 16 L 3 34 C 3 41.17 8.83 47 16 47 L 34 47 C 41.17 47 47 41.17 47 34 L 47 16 C 47 8.83 41.17 3 34 3 L 16 3 z M 37 11 C 38.1 11 39 11.9 39 13 C 39 14.1 38.1 15 37 15 C 35.9 15 35 14.1 35 13 C 35 11.9 35.9 11 37 11 z M 25 14 C 31.07 14 36 18.93 36 25 C 36 31.07 31.07 36 25 36 C 18.93 36 14 31.07 14 25 C 14 18.93 18.93 14 25 14 z M 25 16 C 20.04 16 16 20.04 16 25 C 16 29.96 20.04 34 25 34 C 29.96 34 34 29.96 34 25 C 34 20.04 29.96 16 25 16 z"></path>
 </svg>
